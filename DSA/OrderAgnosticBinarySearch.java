18919e3ee73d6a881a085b936bfb63050b7ca27f
jeshu-collab <jeshu241125@gmail.com>
2026-08-30 15:00:35 +0530

    OrderAgnosticBinarySearch

---
+++ DSA/OrderAgnosticBinarySearch.java
@@ -0,0 +1,41 @@
+public class OrderAgnosticBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        int target = 2;
+        int ans = orderAgnosticBinarySearch(arr, target);
+        System.out.println(ans);
+    }
+
+    static int orderAgnosticBinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        // find whether the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while (start <= end) {
+            // find middle element
+            // int mid = (start+end)/2; mght give array bound exception as start+end can
+            // exceed the range of int in java
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
